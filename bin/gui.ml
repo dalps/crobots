284c0fbf490b39c2918df2cf9c22dcf22dffa42c
Federico <dalpio23@gmail.com>
2024-01-13 20:37:44 +0100

    Gui textures

--- bin/gui.ml
+++ bin/gui.ml
@@ -1,10 +1,8 @@
 open Crobots
 open Raylib
 
-let background_color = Raylib.Color.raywhite
-
 let font_path = "bin/fonts/monogram.ttf"
-let stat_fontsize = 32
+let stat_fontsize = 28
 let stat_fontsize_f = stat_fontsize |> float_of_int
 let winner_fontsize = 80
 let font_spacing = 1.
@@ -33,6 +31,11 @@ let turret_texture = ref dummy_texture
 let tank_shadow_texture = ref dummy_texture
 let turret_shadow_texture = ref dummy_texture
 let trail_texture = ref dummy_texture
+let box_texture = ref dummy_texture
+let arena_shadow_texture = ref dummy_texture
+
+let text_color = Color.raywhite
+let background_color = Color.create 58 68 102 255
 
 let get_srcrec texture =
   (* Printf.printf "w: %d, h: %d\n" (Texture.width texture)(Texture.height texture); *)
@@ -51,7 +54,8 @@ let load_textures () =
   turret_texture := load_texture "bin/textures/turret.png";
   tank_shadow_texture := load_texture "bin/textures/tank_shadow.png";
   turret_shadow_texture := load_texture "bin/textures/turret_shadow.png";
-  trail_texture := load_texture "bin/textures/trail.png"
+  trail_texture := load_texture "bin/textures/trail.png";
+  box_texture := load_texture "bin/textures/gui.png"
 
 let draw_stat_text text pos_x pos_y color =
   let open Raylib in
@@ -69,10 +73,33 @@ let measure_stat_text text =
   let open Raylib in
   measure_text_ex !stat_font text stat_fontsize_f font_spacing
 
-let padding = 15
+let arena_padding = 15
 let stats_width = 400
 let arena_width = 1000
 
+let arena_texture_width = 96
+let arena_border_thickness = arena_texture_width / 3
+
+let dstrec_arena =
+  Rectangle.create (arena_padding |> float) (arena_padding |> float)
+    (arena_width + (arena_border_thickness * 2) |> float)
+    (arena_width + (arena_border_thickness * 2) |> float)
+
+let npatch_arena =
+  let width = arena_texture_width in
+  let border = arena_border_thickness in
+  NPatchInfo.create
+    (Rectangle.create 0. 0. (width |> float) (width |> float))
+    border border border border NPatchLayout.Nine_patch
+
+let npatch_stat =
+  let width = arena_texture_width in
+  let border = 16 in
+  NPatchInfo.create
+    (Rectangle.create 0. 96. (width |> float) (width |> float))
+    border border border border NPatchLayout.Nine_patch
+
+let padding = arena_padding + arena_border_thickness
 let window_height = arena_width + (2 * padding)
 let window_width = window_height + padding + stats_width
 let window_width_f = window_width |> float_of_int
@@ -99,18 +126,23 @@ let draw_arena () =
     padding
     (padding + (arena_width / 2))
     (padding + arena_width) Color.lightgray;
-  draw_rectangle_lines padding padding arena_width arena_width Color.gray
+  draw_texture_npatch !box_texture npatch_arena dstrec_arena (Vector2.zero ())
+    0. Color.white
 
 let draw_stats i (r : Robot.t) color =
   let pos_x = window_height in
-  let pos_y = padding + ((statbox_height + padding) * i) in
+  let pos_y = arena_padding + ((statbox_height + arena_padding) * i) in
   let col_sep = 150 in
-  let col_padding = 10 in
+  let col_padding = 20 in
+  let row_padding = col_padding + 10 in
   let open Raylib in
-  draw_rectangle pos_x pos_y statbox_width statbox_height (fade color 0.25);
-  draw_rectangle_lines pos_x pos_y statbox_width statbox_height color;
-  draw_stat_text (spr "%d. %s" i r.name) (pos_x + col_padding)
-    (pos_y + col_padding) Color.black;
+  let dstrec =
+    Rectangle.create (pos_x |> float) (pos_y |> float) (statbox_width |> float)
+      (statbox_height |> float)
+  in
+  draw_texture_npatch !box_texture npatch_stat dstrec (Vector2.zero ()) 0. (color_contrast color (-0.));
+  draw_stat_text (spr "%d. %s" i r.name) (pos_x + row_padding)
+    (pos_y + col_padding) text_color;
 
   (if r.status = Robot.DEAD then
      let skull_width = stat_fontsize_f in
@@ -127,33 +159,33 @@ let draw_stats i (r : Robot.t) color =
      draw_texture_pro !skull_texture srcrec dstrec (Vector2.zero ()) 0.
        skull_color);
 
-  draw_stat_text (spr "d%%: %d" r.damage) (pos_x + col_padding)
+  draw_stat_text (spr "d%%: %d" r.damage) (pos_x + row_padding)
     (pos_y + name_sep + (stat_height * 1))
-    Color.black;
+    text_color;
   draw_stat_text
     (spr "sc: %d" r.scan_degrees)
-    (pos_x + col_padding + col_sep)
+    (pos_x + row_padding + col_sep)
     (pos_y + name_sep + (stat_height * 1))
-    Color.black;
+    text_color;
 
-  draw_stat_text (spr "sp: %d" r.speed) (pos_x + col_padding)
+  draw_stat_text (spr "sp: %d" r.speed) (pos_x + row_padding)
     (pos_y + name_sep + (stat_height * 2))
-    Color.black;
+    text_color;
   draw_stat_text (spr "hd: %d" r.heading)
-    (pos_x + col_padding + col_sep)
+    (pos_x + row_padding + col_sep)
     (pos_y + name_sep + (stat_height * 2))
-    Color.black;
+    text_color;
 
   draw_stat_text
     (spr " x: %d" (r.x / Robot.click))
-    (pos_x + col_padding)
+    (pos_x + row_padding)
     (pos_y + name_sep + (stat_height * 3))
-    Color.black;
+    text_color;
   draw_stat_text
     (spr " y: %d" (r.y / Robot.click))
-    (pos_x + col_padding + col_sep)
+    (pos_x + row_padding + col_sep)
     (pos_y + name_sep + (stat_height * 3))
-    Color.black
+    text_color
 
 let draw_endgame result =
   let open Raylib in
@@ -167,12 +199,12 @@ let draw_endgame result =
     (Vector2.create
        ((window_width_f /. 2.) -. (Vector2.x w /. 2.))
        ((window_height_f /. 2.) -. (Vector2.y w /. 2.)))
-    h1 font_spacing Color.black;
+    h1 font_spacing text_color;
   draw_text_ex !winner_font t1
     (Vector2.create
        ((window_width_f /. 2.) -. (Vector2.x t1w /. 2.))
        ((window_height_f /. 2.) -. (Vector2.y t1w /. 2.) +. Vector2.y w))
-    h2 font_spacing Color.black
+    h2 font_spacing text_color
 
 let draw_cycles c =
   let open Raylib in
@@ -181,7 +213,7 @@ let draw_cycles c =
   draw_stat_text text
     (window_width - (Vector2.x v |> int_of_float) - padding)
     (window_height - (Vector2.y v |> int_of_float) - padding)
-    Color.black
+    text_color
 
 let draw_fps n =
   let open Raylib in
@@ -190,4 +222,4 @@ let draw_fps n =
   draw_stat_text text
     (window_width - (Vector2.x v |> int_of_float) - padding)
     (window_height - ((Vector2.y v |> int_of_float) * 2) - padding)
-    Color.black
+    text_color
